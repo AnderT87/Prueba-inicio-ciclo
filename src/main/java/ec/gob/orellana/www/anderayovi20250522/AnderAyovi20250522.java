67a3e3a1810753120ff71c013793bdd49bdaebed
AnderT87 <anderayov@gmail.com>
2025-05-22 17:38:54 -0500

    PruebaMedioCiclo

---
+++ src/main/java/ec/gob/orellana/www/anderayovi20250522/AnderAyovi20250522.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.gob.orellana.www.anderayovi20250522;
+
+/**
+ *
+ * @author SO-LAB-PC6
+ */
+public class AnderAyovi20250522 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
